7032015f29eb85cb8f51398a904727aed3607f73
Natalia Braga da Fonseca - NFO <natalia.fonseca@iteris.com.br>
2022-05-30 19:40:42 -0300

    feat: desafio tautograma

---
+++ Tests.Intro/Tautograma.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Intro
+{
+    public class Tautograma
+    {
+        public static string Validar(string entrada)
+        {
+            if (entrada == null)
+            {
+                throw new ArgumentException("Frase inválida.");
+            }
+
+            string[] EntradaArray = entrada.Trim().Split();
+
+            if (EntradaArray.Length < 2)
+            {
+                throw new ArgumentException("Frase inválida.");
+            }
+
+            char CaractereInicial = entrada.ToLower()[0];
+            foreach (string palavra in EntradaArray)
+            {
+                if (palavra.ToLower()[0] != CaractereInicial)
+                {
+                    return "N";
+                }
+            }
+
+            return "S";
+        }
+    }
+}
